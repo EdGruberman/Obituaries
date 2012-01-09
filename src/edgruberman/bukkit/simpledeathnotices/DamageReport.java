511aa5aa57d9fd80dd4ec581b6564ad0ef118d6d
EdGruberman <ed@rjump.com>
2012-01-09 01:31:06 -0700

    FIX: Death by fire events not properly identifying combuster

--- src/edgruberman/bukkit/simpledeathnotices/DamageReport.java
+++ src/edgruberman/bukkit/simpledeathnotices/DamageReport.java
@@ -1,8 +1,12 @@
 package edgruberman.bukkit.simpledeathnotices;
 
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
@@ -25,6 +29,12 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.material.MaterialData;
 
 final class DamageReport {
+    
+    final static Set<Material> COMBUSTIBLES = new HashSet<Material>(Arrays.asList(new Material[] {
+              Material.LAVA
+            , Material.STATIONARY_LAVA
+            , Material.FIRE
+    }));
     
     static Map<Entity, DamageReport> last = new HashMap<Entity, DamageReport>();
     static Map<Entity, String> combuster = new HashMap<Entity, String>();
@@ -179,10 +189,75 @@ final class DamageReport {
             return;
         }
         
+//        System.out.println(event instanceof EntityCombustByBlockEvent);
+//        try { throw new Exception(); } catch (Exception e) { e.printStackTrace(); }
+        
         if (event instanceof EntityCombustByBlockEvent) {
             EntityCombustByBlockEvent byBlock = (EntityCombustByBlockEvent) event;
             DamageReport.combuster.put(event.getEntity(), DamageReport.describeMaterial(byBlock.getCombuster().getState().getData()));
             return;
         }
+        
+        // Assume block
+        
+        // Check current
+        Location original = event.getEntity().getLocation();
+        if (DamageReport.identifyCombuster(original.clone(), event.getEntity())) return;
+        
+        // Check closest on x
+        Double adjustX = DamageReport.closestAdjustOnAxis(original.getX());
+        if (adjustX != null)
+            if (DamageReport.identifyCombuster(original.clone().add(adjustX, 0, 0), event.getEntity()))
+                return;
+        
+        // Check closest on z
+        Double adjustZ = DamageReport.closestAdjustOnAxis(original.getZ());
+        if (adjustZ != null)
+            if (DamageReport.identifyCombuster(original.clone().add(0, 0, adjustZ), event.getEntity()))
+                return;
+        
+        // Check closest diagonal
+        if (adjustX != null && adjustZ != null)
+            if (DamageReport.identifyCombuster(original.clone().add(adjustX, 0, adjustZ), event.getEntity()))
+                return;
+
+    }
+    
+    private static Double closestAdjustOnAxis(double coord) {
+        double fPart = Math.abs(coord - (long) coord);
+        if (fPart == 0.5d) return null;
+        
+        return (fPart < 0.5d ? -1 : 1) * Math.signum(coord);
+    }
+    
+    private static boolean identifyCombuster(final Location location, final Entity entity) {
+        // Check foot block
+        if (DamageReport.isCombustible(location)) {
+            DamageReport.combuster.put(entity, DamageReport.describeMaterial(location.getBlock().getState().getData()));
+            return true;
+        }
+        
+        // Check head block
+        location.add(0, 1, 0);
+        if (DamageReport.isCombustible(location)) {
+            DamageReport.combuster.put(entity, DamageReport.describeMaterial(location.getBlock().getState().getData()));
+            return true;
+        }
+        
+        // Check block above head if high enough
+        double fPartY = location.getY() - (long) location.getY();
+        if (fPartY > (2d - 1.62d)) {
+            location.add(0, 1, 0);
+            if (DamageReport.isCombustible(location)) {
+                DamageReport.combuster.put(entity, DamageReport.describeMaterial(location.getBlock().getState().getData()));
+                return true;
+            }
+        }
+        
+        return false;
+    }
+    
+    private static boolean isCombustible(final Location location) {
+        return DamageReport.COMBUSTIBLES.contains(location.getBlock().getType());
     }
 }
